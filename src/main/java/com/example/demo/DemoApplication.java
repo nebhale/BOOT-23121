52320b85a6294b74dbc5b23de924cad49015d239
Ben Hale <bhale@vmware.com>
2020-08-28 09:23:45 -0700

    Initial Commit
    Signed-off-by: Ben Hale <bhale@vmware.com>

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		BlockHound.install();
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
